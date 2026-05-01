a7fca01e4ef2c750e9657ff885ca900c23f7f5dc
Lucas <lucasteofilodev@gmail.com>
2026-05-01 17:52:36 -0300

    Add project files.

---
+++ BankSimple/Conta.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSimple
+{
+    internal class Conta
+    {
+        public int NumConta { get; private set; }
+        private string _nome;
+        private double _saldo;
+
+
+        public Conta(int numconta, string nome)
+        {
+            NumConta = numconta;
+            _nome = nome;
+        }
+        public Conta(int numconta, string nome, double saldo) : this(numconta, nome)
+        {
+            
+            _saldo = saldo;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public void Deposito(double deposito)
+        {
+            if(deposito <= 0)
+            {
+                Console.WriteLine("Error! Valor menor ou igual a zero");
+            } else
+            {
+                _saldo += deposito;
+            }
+        }
+
+        public void Saque(double saque)
+        {
+            if(saque <= 0)
+            {
+                Console.WriteLine("Error! Valor menor ou igual a zero");
+            } else
+            {
+                _saldo = ((_saldo - saque) - 5.0);
+                
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumConta
+                + ", Tituar: "
+                + _nome
+                + ", Saldo: $ "
+                + _saldo;
+        }
+    }
+}
---
+++ BankSimple/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace BankSimple.Main
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Entre o número da conta: ");
+            int numconta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (s/n) ? ");
+            string escolha = Console.ReadLine();
+            escolha = escolha.Trim(); // remover os espaços
+            escolha = escolha.ToLower(); //deixar tudo minusculo
+
+            Conta c;
+
+            if (escolha == "s")
+            {
+                Console.WriteLine("Entre com o valor depósito inicial:");
+                double deposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                c = new Conta(numconta, nome, deposito);
+            } else
+            {
+                c = new Conta(numconta, nome);
+            }
+
+            int opção = 0;
+
+            do
+            {
+                Console.WriteLine(c);
+                Console.WriteLine("-----------");
+                Console.WriteLine("Menu de opções");
+                Console.WriteLine("1 - Deposito");
+                Console.WriteLine("2 - Saque (Obs: Taxa de R$ 5.00 por saque)");
+                Console.WriteLine("3 - Alterar Dado Cadrastral (Obs: Nome)");
+                Console.WriteLine("4 - Sair");
+                Console.WriteLine("-----------");
+                Console.WriteLine("Digite a opção escolhida:");
+                opção = int.Parse(Console.ReadLine());
+
+                switch (opção)
+                {
+                    case 1:
+
+                        Console.WriteLine("Entre com o valor depósito:");
+                        double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        c.Deposito(deposito);
+
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Entre com o valor saque: (Obs:Taxa de R$ 5.00 por saque)");
+                        double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        c.Saque(saque);
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Entre com o novo nome a ser cadastrado: ");
+                        string novonome = Console.ReadLine();
+                        c.Nome = novonome;
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Saindo...");
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Valor digitado invalido...");
+
+                        break;
+                }
+
+                
+            } while (opção != 4);
+
+            
+        }
+    }
+
+}
